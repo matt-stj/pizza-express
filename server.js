89052532b87349932458d7829e37e4480753b0b8
Matt Stjernholm <thew88@gmail.com>
2016-03-01 13:10:39 -0700

    rendering views

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const app = express();
+
 const path = require('path');
+
+app.use(express.static('static'));
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Pizza Express';
+
+app.get('/', (request, response) => {
+  response.render('index');
+});
+
+app.set('view engine', 'jade');
 
 if (!module.parent) {
   app.listen(app.get('port'), () => {
     console.log(`${app.locals.title} is running on ${app.get('port')}.`);
   });
 }
-
-app.set('port', process.env.PORT || 3000);
-app.locals.title = 'Pizza Express';
-
-app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, '/static/index.html'));
-});
-
-app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
-});
-
 
 module.exports = app;
