847c5f9a774c96f6cb76165e9024c35e9314d82e
Matt Stjernholm <thew88@gmail.com>
2016-03-01 13:56:56 -0700

    starting generating unique keys

--- server.js
+++ server.js
@@ -2,17 +2,24 @@ const express = require('express');
 const app = express();
 
 const path = require('path');
+const bodyParser = require('body-parser');
+
+app.locals.pizzas = {};
 
 app.use(express.static('static'));
 
 app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'jade');
 app.locals.title = 'Pizza Express';
 
 app.get('/', (request, response) => {
   response.render('index');
 });
 
-app.set('view engine', 'jade');
+app.post('/pizzas', (request, response) => {
+  response.sendStatus(201);
+});
+
 
 if (!module.parent) {
   app.listen(app.get('port'), () => {
